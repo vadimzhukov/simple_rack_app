ac438841ca9d8ff650b29c1a1cbcf89f488d01d6
VadimZhukov <vadim.zhukov@gmail.com>
2023-01-17 13:37:52 +0300

    fix: timer formatter to separate class

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
+require_relative "time_formatter"
 class App
-  ALLOWED_PARAMS = %w[year month day hour minute second]
-  DATE_FORMAT = { year: '%Y', month: '%m', day: '%d', hour: '%H', minute: '%M', second: '%S' }
+  
   PROTOCOL = 'HTTP/1.1'
   HOST = 'localhost:9292'
   PATH = '/time'
@@ -8,14 +8,15 @@ class App
 
   def call(env)
     if check_request(env)
-      status = set_status
-      body = [request_convert(env)]
+      time = TimeFormatter.new(env)
+      status = time.convert_format[:status]
+      body = [time.convert_format[:body]]
     else
       status = 404
       body = ['Bad request']
     end
 
-    [status, headers, body]
+    [status, headers, body] 
   end
 
   private
@@ -38,22 +39,6 @@ class App
 
   def check_host(env)
     env['HTTP_HOST'] == HOST
-  end
-
-  def request_convert(env)
-    query_str = Rack::Utils.unescape(env['QUERY_STRING'])
-    params_key = query_str.split('=')[0]
-    params_values_array = query_str.split('=')[1].split(',')
-    if params_key == 'format' && (params_values_array & ALLOWED_PARAMS).count == params_values_array.count
-      format_type = params_values_array.map { |i| i = DATE_FORMAT[i.to_sym] }.join('-')
-      Time.now.strftime(format_type)
-    else
-      "Unknown time format: #{params_values_array - ALLOWED_PARAMS}"
-    end
-  end
-
-  def set_status
-    200
   end
 
   def headers
